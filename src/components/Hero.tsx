1cc8d15183589aa178eddc9fde54a7de005a49d3
Harsh <myemail@example.com>
2025-01-08 15:53:33 +0530

    Initial Setup

---
+++ src/components/Hero.tsx
@@ -0,0 +1,16 @@
+const Hero = () => {
+  return (
+    <div className="relative h-dvh w-screen overflow-x-hidden">
+      <div
+        id="vide-frame"
+        className="relative z-10 w-screen h-dvh overflow-hidden rounded-lg blue75
+      "
+      >
+        <div>
+          <div className="mask-clip-path"></div>
+        </div>
+      </div>
+    </div>
+  );
+};
+export default Hero;
